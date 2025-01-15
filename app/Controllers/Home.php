09effdc6d156079f8a689fa00a04e85f749019ca
Elsarizqiana23 <akaelsa4@gmail.com>
2025-01-15 18:59:41 +0700

    Add files via upload

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,37 +1,11 @@
 <?php
 
 namespace App\Controllers;
-use App\Models\UserModel;
-use App\Models\PeminjamanModel;
-use App\Models\BukuModel;
 
 class Home extends BaseController
 {
-    protected $peminjamanModel;
-    protected $userModel;
-    protected $bukuModel;
-
-    public function __construct()
+    public function index(): string
     {
-        $this->userModel = new UserModel();
-        $this->peminjamanModel = new PeminjamanModel();
-        $this->bukuModel = new BukuModel();
-    }
-    public function index()
-    {
-        if (!session()->get('isLoggedIn')) {
-            return redirect()->to(base_url('login'));
-        }
-        if (session()->get('role') == '1') {
-            return redirect()->to(base_url('petugas'));
-        }
-
-        $id_peminjam = session()->get('id');
-
-        return view('user/index', [
-            'nama' => session()->get('nama'),
-            'totalBuku' => $this->bukuModel->countAllResults(),
-            'totalPeminjaman' => count($this->peminjamanModel->getPeminjamanWithBuku($id_peminjam)),
-        ]);
+        return view('welcome_message');
     }
 }
